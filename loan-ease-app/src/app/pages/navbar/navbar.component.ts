cc2550ecbfcbc71a25221ca6cf01b96b5f647edb
Mukul Dalal <mukuldalalnfs@gmail.com>
2025-01-15 11:04:12 +0530

    Enh: #15 - Implement auto-hide/expand navbar

--- loan-ease-app/src/app/pages/navbar/navbar.component.ts
+++ loan-ease-app/src/app/pages/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,6 +8,27 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+    let prevScrollPosition = window.pageYOffset;
+    let navbar = document.getElementById('navbar');
+
+    window.addEventListener('scroll', () => {
+      let currentScrollPosition = window.pageYOffset;
+
+      if (navbar) {
+        if (currentScrollPosition > prevScrollPosition) {
+          // scrolling down
+          navbar.style.transform = 'translateY(-100%)';
+        } else {
+          // scrolling up
+          navbar.style.transform = 'translateY(0)';
+        }
+      }
+
+      prevScrollPosition = currentScrollPosition;
+    });
+  }
 
 }
